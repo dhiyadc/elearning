746f00b93e903822b003991e728f33299b294811
mtiansyahp <m.tiansyahp@yahoo.com>
2020-05-10 21:35:15 +0700

    banner

--- application/views/tampilankelas/kelasview.php
+++ application/views/tampilankelas/kelasview.php
@@ -7,22 +7,13 @@
 
 
     
-      <div class="site-section courses-title" id="courses-section">
-      <div class="container">
-        <div class="row mb-5 justify-content-center">
-          <div class="col-lg-7 text-center" data-aos="fade-up" data-aos-delay="">
-            <h2 class="section-title"></h2>
-          </div>
-        </div>
-      </div>
+  <div class="row">   
+    <div class="col-lg-16 bg-dark">
+
     </div>
-    <div class="site-section courses-entry-wrap"  data-aos="fade-up" data-aos-delay="100">
-      <div class="container">
-          <div class="material">
+  </div>
 
-          </div>
-
-        <div class="row">
+        <!-- <div class="row">
         <div class="container">
         <div class="row">
             <div class="col-lg-12 col-sm-6" data-aos="fade-up" data-aos-delay="100">
@@ -41,7 +32,7 @@
             
             </div>
         </div>
-    </div>
+    </div> -->
 
     <div class="container">
       <div class="row mb-5" >
@@ -342,8 +333,7 @@
             <button class="customNextBtn btn btn-primary m-1">Next</button>
           </div>
         </div>
-      </div>
-    </div>
+     
 
 
     
@@ -365,55 +355,6 @@
     </div>
 
 
-    <div class="site-section bg-light" id="contact-section">
-      <div class="container">
 
-        <div class="row justify-content-center">
-          <div class="col-md-7">
-
-
-            
-            <h2 class="section-title mb-3">Message Us</h2>
-            <p class="mb-5">Natus totam voluptatibus animi aspernatur ducimus quas obcaecati mollitia quibusdam temporibus culpa dolore molestias blanditiis consequuntur sunt nisi.</p>
-          
-            <form method="post" data-aos="fade">
-              <div class="form-group row">
-                <div class="col-md-6 mb-3 mb-lg-0">
-                  <input type="text" class="form-control" placeholder="First name">
-                </div>
-                <div class="col-md-6">
-                  <input type="text" class="form-control" placeholder="Last name">
-                </div>
-              </div>
-
-              <div class="form-group row">
-                <div class="col-md-12">
-                  <input type="text" class="form-control" placeholder="Subject">
-                </div>
-              </div>
-
-              <div class="form-group row">
-                <div class="col-md-12">
-                  <input type="email" class="form-control" placeholder="Email">
-                </div>
-              </div>
-              <div class="form-group row">
-                <div class="col-md-12">
-                  <textarea class="form-control" id="" cols="30" rows="10" placeholder="Write your message here."></textarea>
-                </div>
-              </div>
-
-              <div class="form-group row">
-                <div class="col-md-6">
-                  
-                  <input type="submit" class="btn btn-primary py-3 px-5 btn-block btn-pill" value="Send Message">
-                </div>
-              </div>
-
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
     
      
