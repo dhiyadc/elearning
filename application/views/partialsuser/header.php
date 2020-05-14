5e921a9a5d851800d9836a8477909ae7e4055377
mtiansyahp <m.tiansyahp@yahoo.com>
2020-05-14 18:53:29 +0700

    view sementara

--- application/views/partialsuser/header.php
+++ application/views/partialsuser/header.php
@@ -51,8 +51,8 @@
           <div class="mx-auto text-center">
             <nav class="site-navigation position-relative text-right" role="navigation">
               <ul class="site-menu main-menu js-clone-nav mx-auto d-none d-lg-block  m-0 p-0">
-                <li><a href="#" class="nav-link">Beranda</a></li>
-                <li><a href="#" class="nav-link">Kelas</a></li>
+                <li><a href="<?php echo base_url(); ?>" class="nav-link">Beranda</a></li>
+                <li><a href="Kelas" class="nav-link">Kelas</a></li>
                 <li><a href="#" class="nav-link">Profile</a></li>
                
               </ul>
@@ -62,7 +62,7 @@
           <div class="ml-auto w-25">
             <nav class="site-navigation position-relative text-right" role="navigation">
               <ul class="site-menu main-menu site-menu-dark js-clone-nav mr-auto d-none d-lg-block m-0 p-0">
-                <li class="cta"><a href="#contact-section" class="nav-link"><span>Keluar</span></a></li>
+                <li class="cta"><a href="<?php echo base_url(); ?>" class="nav-link"><span>Keluar</span></a></li>
               </ul>
             </nav>
             <a href="#" class="d-inline-block d-lg-none site-menu-toggle js-menu-toggle text-black float-right"><span class="icon-menu h3"></span></a>
--- application/views/user/user_view.php
+++ application/views/user/user_view.php
@@ -14,53 +14,53 @@
     <!--Grid column-->
     <div class="col-md-12 mb-4">
 
-      <h5 class="text-center font-weight-bold mb-4">Dashboard Saya</h5>
+        <h5 class="text-center font-weight-bold mb-4">Dashboard Saya</h5>
 
-      <div class="d-flex justify-content-between">
-        <small class="text-muted">Add products to cart</small>
-        <small><span><strong>160</strong></span>/<span></span>200</small>
-      </div>
-      <div class="progress md-progress">
-        <div class="progress-bar bg-success" role="progressbar" style="width: 25%" aria-valuenow="25"
-          aria-valuemin="0" aria-valuemax="100"></div>
-      </div>
+        <div class="d-flex justify-content-between">
+          <small class="text-muted">Add products to cart</small>
+          <small><span><strong>160</strong></span>/<span></span>200</small>
+        </div>
+        <div class="progress md-progress">
+          <div class="progress-bar bg-success" role="progressbar" style="width: 25%" aria-valuenow="25"
+            aria-valuemin="0" aria-valuemax="100"></div>
+        </div>
 
-      <div class="d-flex justify-content-between">
-        <small class="text-muted">Complete Purchase</small>
-        <small><span><strong>310</strong></span>/<span></span>400</small>
-      </div>
-      <div class="progress md-progress">
-        <div class="progress-bar bg-info" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0"
-          aria-valuemax="100"></div>
-      </div>
+        <div class="d-flex justify-content-between">
+          <small class="text-muted">Complete Purchase</small>
+          <small><span><strong>310</strong></span>/<span></span>400</small>
+        </div>
+        <div class="progress md-progress">
+          <div class="progress-bar bg-info" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0"
+            aria-valuemax="100"></div>
+        </div>
 
-      <div class="d-flex justify-content-between">
-        <small class="text-muted">Visit Premium Page</small>
-        <small><span><strong>480</strong></span>/<span></span>800</small>
-      </div>
-      <div class="progress md-progress">
-        <div class="progress-bar bg-warning" role="progressbar" style="width: 75%" aria-valuenow="75"
-          aria-valuemin="0" aria-valuemax="100"></div>
-      </div>
+        <div class="d-flex justify-content-between">
+          <small class="text-muted">Visit Premium Page</small>
+          <small><span><strong>480</strong></span>/<span></span>800</small>
+        </div>
+        <div class="progress md-progress">
+          <div class="progress-bar bg-warning" role="progressbar" style="width: 75%" aria-valuenow="75"
+            aria-valuemin="0" aria-valuemax="100"></div>
+        </div>
 
-      <div class="d-flex justify-content-between">
-        <small class="text-muted">Send Inquiries</small>
-        <small><span><strong>250</strong></span>/<span></span>500</small>
-      </div>
-      <div class="progress md-progress">
-        <div class="progress-bar bg-danger" role="progressbar" style="width: 100%" aria-valuenow="100"
-          aria-valuemin="0" aria-valuemax="100"></div>
-      </div>
+        <div class="d-flex justify-content-between">
+          <small class="text-muted">Send Inquiries</small>
+          <small><span><strong>250</strong></span>/<span></span>500</small>
+        </div>
+        <div class="progress md-progress">
+          <div class="progress-bar bg-danger" role="progressbar" style="width: 100%" aria-valuenow="100"
+            aria-valuemin="0" aria-valuemax="100"></div>
+        </div>
 
+      </div>
+      <!--Grid column-->
+
+      
     </div>
-    <!--Grid column-->
+    <!--Grid row-->
 
-    
-  </div>
-  <!--Grid row-->
-
-</section>
-<!--Section: Block Content-->
+  </section>
+  <!--Section: Block Content-->
 
 
 </div>
