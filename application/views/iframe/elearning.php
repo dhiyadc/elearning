4e32bd7a2a481292c3dae9b23a6372f8045d4b5e
mtiansyahp <m.tiansyahp@yahoo.com>
2020-05-23 16:30:12 +0700

    iframe add

---
+++ application/views/iframe/elearning.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Elearn</title>
+
+    <style type="text/css"> 
+        html { 
+            overflow: auto; 
+        } 
+          
+        html, 
+        body, 
+        div, 
+        iframe { 
+            margin: 0px; 
+            padding: 0px; 
+            height: 100%; 
+            border: none; 
+        } 
+          
+        iframe { 
+            display: block; 
+            width: 100%; 
+            border: none; 
+            overflow-y: auto; 
+            overflow-x: hidden; 
+        } 
+    </style> 
+
+</head>
+<body>
+    <iframe 
+            allow="camera; microphone"
+            src="https://meet.jit.si/testthings"
+            frameborder="0" 
+            marginheight="0" 
+            marginwidth="0" 
+            width="100%" 
+            height="100%" 
+            scrolling="auto"
+            allowfullscreen> 
+  </iframe> 
+</body>
+</html>
