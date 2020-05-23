9cb3d563d863d5e431248480d967bc74338d5c7d
mtiansyahp <m.tiansyahp@yahoo.com>
2020-05-23 08:41:06 +0700

    lupapassword

--- application/views/user/forgot_password.php
+++ application/views/user/forgot_password.php
@@ -1,54 +1,89 @@
 
 <?php $this->load->view('partials/header'); ?>
 <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"> -->
-<div class="intro-section" id="home-section">
-      
-      <div class="slide-1 gambar1" data-stellar-background-ratio="0.5">
-        <div class="container">
-          <div class="row align-items-center">
-            <div class="col-12">
-              <div class="row align-items-center">
-                
-              <div class="col-lg-12 ml-auto mt-5 mb-4" data-aos="fade-up" data-aos-delay="500">
-                  <form action="<?= base_url(); ?>forgot_password/request" method="post" class="form-box">
-                    <h3 class="h4 text-black mb-4 text-center">Reset Password</h3>
-                    <?php
-                    if (isset($message_display)) {
-                    echo "<div class='message'>";
-                    echo $message_display;
-                    echo "</div>";
-                    ?>
-                    <a href="<?= base_url(); ?>login">Back to login</a>
-                    <?php
-                    } else {
-                    ?>
-                      <div class="form-group row">
-                            <label class="control-label col-md-2">Email</label>
-                              <div class="col-md-8">
-                                  <input class="form-control" type="email" name="email" placeholder="Your Email Address">
-                              </div>
-                      </div>
-                      <div class="form-group text-center">
-                      
-                        <button class="btn btn-primary shadow btn-pill" type="submit">Kirim Permintaan</button>
-                        <a href="<?= base_url(); ?>login" class="btn btn-light btn-pill">Back</a>
-                        <?php
-                        }
+<section class="user_dashboard">
+<div class="row mt-0">
+  <div class="col-lg-12" style="background-color: aquamarine;" >
+    <div class="card" style="border-radius: 0px; background-color: darkcyan;"> 
+    <div class="container my-5 pt-5 pb-3 px-4 z-depth-1">
 
-                        echo "<div class='error_msg'>";
-                        if (isset($error_message)) {
-                        echo $error_message;
-                        }
-                        echo "</div>";
-                ?>
-                      </div>
-              </div>
-            </div>
-            
-          </div>
-        </div>
+
+<!--Section: Block Content-->
+<section>
+
+  <!--Grid row-->
+  <div class="row">
+
+    <!--Grid column-->
+    <div class="col-md-12 mb-4">
+
+        <h5 class="text-center font-weight-bold mb-4" style="color: white">Pengaturan Akun</h5>
+
+
       </div>
+      <!--Grid column-->
+
+      
     </div>
+    <!--Grid row-->
+
+  </section>
+  <!--Section: Block Content-->
+
+
+</div>
+    </div>
+  </div>
+</div>
+
+
+  
+</div>
+ 
+<div class="container my-5">
+
+
+<div class="container">
+    <form class="form-horizontal" role="form" method="POST" action="/register">
+        <div class="row">
+            <div class="col-md-3 mb-3"><h2>Lupa Password</h2><hr></div>
+            <div class="col-md-6">
+                
+            </div>
+        </div>
+       
+        <div class="row">
+            <div class="col-md-3 field-label-responsive">
+                <label for="newpassword">Email</label>
+            </div>
+            <div class="col-md-6">
+                <div class="form-group">
+                    <div class="input-group mb-2 mr-sm-2 mb-sm-0">
+                      
+                        <input type="email" name="newpass" class="form-control" id="emailver"
+                               placeholder="" required autofocus>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="form-control-feedback">
+                        <span class="text-danger align-middle">
+                            <!-- Put e-mail validation error messages here -->
+                        </span>
+                </div>
+            </div>
+        </div>
+       
+        
+        <div class="row">
+            <div class="col-md-3"></div>
+            <div class="col-md-6">
+                <button type="submit" class="btn btn-primary"><i class="fa fa-user-plus"></i>Kirim</button>
+            </div>
+        </div>
+    </form>
+</div>
+</section>
     <!-- <script src="http://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
     <script type="text/javascript">
         var email = $("#email");
