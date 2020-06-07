470a48690201e16e8d067a5ba9d087b64c519867
mtiansyahp <m.tiansyahp@yahoo.com>
2020-06-07 20:50:26 +0700

    rapiin button open class

--- application/views/classes/open_class.php
+++ application/views/classes/open_class.php
@@ -177,8 +177,8 @@
                                     <?php elseif ($cek == false) : ?>
                                     <?php endif; ?>
                                   <?php elseif ($val2['status_kegiatan'] != CLASS_FINISHED) : ?>
-                                      <button type="button" class="btn btn-light" data-toggle="modal" data-target="#editKegiatan<?= $val2['id_kegiatan']; ?>">Edit</button><br>
-                                      <a href="<?= base_url('class/') ?><?= $val['id_kelas'] ?>/<?= $val2['id_kegiatan']; ?>" class="btn btn-dark mr-1">Mulai</a>
+                                      <button type="button" class="btn btn-light btn-block" data-toggle="modal" data-target="#editKegiatan<?= $val2['id_kegiatan']; ?>">Edit</button><br>
+                                      <a href="<?= base_url('class/') ?><?= $val['id_kelas'] ?>/<?= $val2['id_kegiatan']; ?>" class="btn btn-dark mr-1 btn-block">Mulai</a>
                                       <div class="modal fade" id="editKegiatan<?= $val2['id_kegiatan']; ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" style="padding-right: 90px;">
                                         <div class="modal-dialog" role="document">
                                           <!--Content-->
