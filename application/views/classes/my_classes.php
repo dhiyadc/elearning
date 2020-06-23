7615f727d8a0202183567736b16fdfcd6d133a5b
mtiansyahp <m.tiansyahp@yahoo.com>
2020-06-23 22:28:05 +0700

    collapes

--- application/views/classes/my_classes.php
+++ application/views/classes/my_classes.php
@@ -411,7 +411,7 @@
 								</nav>
 							</div>
 						</div>
-
+														
 					</div>
 				</div>
 
@@ -513,6 +513,52 @@
 						</div>
 					</div>
 				</div>
+					<!--  -->
+					<div id="accordion" role="tablist">
+						<div class="card">
+							<div class="card-header" role="tab" id="headingOne">
+							<h5 class="mb-0">
+								<a data-toggle="collapse" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+								Collapsible Group Item #1
+								</a>
+							</h5>
+						</div>
+						<div id="collapseOne" class="collapse show" role="tabpanel" aria-labelledby="headingOne" data-parent="#accordion">
+							<div class="card-body">
+								
+							</div>
+						</div>
+						</div>
+						<div class="card">
+							<div class="card-header" role="tab" id="headingTwo">
+							<h5 class="mb-0">
+								<a class="collapsed" data-toggle="collapse" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+								Collapsible Group Item #2
+								</a>
+							</h5>
+						</div>
+						<div id="collapseTwo" class="collapse" role="tabpanel" aria-labelledby="headingTwo" data-parent="#accordion">
+							<div class="card-body">
+								...............................................................................#2
+							</div>
+						</div>
+						</div>
+						<div class="card">
+							<div class="card-header" role="tab" id="headingThree">
+							<h5 class="mb-0">
+								<a class="collapsed" data-toggle="collapse" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+								Collapsible Group Item #3
+								</a>
+							</h5>
+						</div>
+						<div id="collapseThree" class="collapse" role="tabpanel" aria-labelledby="headingThree" data-parent="#accordion">
+							<div class="card-body">
+							...............................................................................#3
+							</div>
+						</div>
+						</div>
+					</div>
+															<!--  -->
 			</div>
 
 			<div class="tab-pane" id="tab3" role="tabpanel" aria-expanded="false">
