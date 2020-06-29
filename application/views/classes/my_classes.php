125e579900f425e422028b395afd040023655139
zorayaw <zorayaw31@gmail.com>
2020-06-29 18:13:58 +0700

    edit merger err

--- application/views/classes/my_classes.php
+++ application/views/classes/my_classes.php
@@ -631,19 +631,6 @@
 									</div>
 								</div>
 							</div>
-							<input type="hidden" id="dtp_input1" />
-							<div class="form-group">
-								<label for="materiForm">Materi (Opsional)</label>
-								<input type="file" name="materi[]" accept=".doc, .docx, .ppt, .pptx, .pdf" class="form-control-file" id="materiForm" multiple>
-							</div>
-							<div class="text-center mb-3">
-								<button type="submit" class="btn btn-light blue-gradient btn-block btn-rounded z-depth-1a">Simpan</button>
-							</div>
-						</form>
-					</div>
-				</div>
-			</div>
-		</div>
 
 
 
