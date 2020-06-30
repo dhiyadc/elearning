eb5f7dd159729e1305bbefd2555f6ee044d7844d
mtiansyahp <m.tiansyahp@yahoo.com>
2020-07-01 01:54:42 +0700

    gambar

--- application/views/home.php
+++ application/views/home.php
@@ -524,10 +524,10 @@
       <div class="container">
         <div class="row justify-content-center align-items-center">
           <div class="col-md-8 text-center testimony">
-            <img src="<?php echo base_url(); ?>assets/images/person_2.jpg" alt="Image" class="img-fluid w-25 mb-4 rounded-circle">
-            <h3 class="mb-4">Jerome Jensen</h3>
+            <img src="<?php echo base_url(); ?>assets/images/original.jpg" alt="Image" class="img-fluid w-25 mb-4 rounded-circle">
+            <h3 class="mb-4">Setsuna F Say Yeah</h3>
             <blockquote>
-              <p>&ldquo; Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum rem soluta sit eius necessitatibus voluptate excepturi beatae ad eveniet sapiente impedit quae modi quo provident odit molestias! Rem reprehenderit assumenda &rdquo;</p>
+              <p>&ldquo; Teknologi adalah Harapan bagi mMimpi Para Anak muda di esok hari kelak &rdquo;</p>
             </blockquote>
           </div>
         </div>
