a599a9e78ab8d3d3d8a1e2427238ef3d2c2011c0
aryapradata <aryapradata@gmail.com>
2020-06-30 20:11:38 +0700

    .

--- application/views/home.php
+++ application/views/home.php
@@ -290,7 +290,7 @@
       <div class="container">
         <div class="row justify-content-center align-items-center">
           <div class="col-md-8 text-center testimony">
-            <img src="<?php echo base_url(); ?>assets/images/person_4.jpg" alt="Image" class="img-fluid w-25 mb-4 rounded-circle">
+            <img src="<?php echo base_url(); ?>assets/images/person_2.jpg" alt="Image" class="img-fluid w-25 mb-4 rounded-circle">
             <h3 class="mb-4">Jerome Jensen</h3>
             <blockquote>
               <p>&ldquo; Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum rem soluta sit eius necessitatibus voluptate excepturi beatae ad eveniet sapiente impedit quae modi quo provident odit molestias! Rem reprehenderit assumenda &rdquo;</p>
