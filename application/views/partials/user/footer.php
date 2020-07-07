57f89d283bd9b32cb58befbdd2933fca1d57627e
mtiansyahp <m.tiansyahp@yahoo.com>
2020-07-07 20:27:56 +0700

    benenrin copyright

--- application/views/partials/user/footer.php
+++ application/views/partials/user/footer.php
@@ -34,7 +34,7 @@
             <div class="border-top pt-5">
             <p>
         <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-        Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved <a href="" target="_blank" >Digital Creative</a>
+        Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved <a href="" target="_blank" >CLASSICO</a>
         <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
       </p>
             </div>
