8bf7bc19260a58cb9d023dab87c1058e05af4f55
mtiansyahp <m.tiansyahp@yahoo.com>
2020-07-07 17:31:00 +0700

    bug akademik sidebar mobile lebih fix

--- application/views/classes/my_classes.php
+++ application/views/classes/my_classes.php
@@ -8,7 +8,7 @@
 <link href="<?= base_url() ?>assets/css/dataTables/dataTables.responsive.css" rel="stylesheet"> -->
 
 <section class="user_dashboard">
-	<div class="row mt-0">
+
 		<div class="col-lg-12" style="background-color: darkcyan;">
 			<div class="card" style="border-radius: 0px; background-color: darkcyan;">
 				<div class="container my-5 pt-5 pb-3 px-4 z-depth-1">
@@ -42,7 +42,7 @@
 				</div>
 			</div>
 		</div>
-	</div>
+
 
 
 
