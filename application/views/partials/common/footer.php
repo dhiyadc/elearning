12a33cbe243454c40872f6f3f01a7480e1626466
mtiansyahp <m.tiansyahp@yahoo.com>
2020-07-12 17:02:37 +0700

    rapiin footer

--- application/views/partials/common/footer.php
+++ application/views/partials/common/footer.php
@@ -33,9 +33,9 @@
           <div class="col-md-12">
             <div class="border-top pt-5">
             <p>
-        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+
         Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved <a href="" target="_blank" >CLASSICO</a>
-        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+  
       </p>
             </div>
           </div>
